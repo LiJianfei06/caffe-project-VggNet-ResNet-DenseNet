495f396c8ebb00e1b56f1f16c1b234a3347bfd6b
LiJianfei06 <976491174@qq.com>
2018-10-12 19:28:51 +0800

    20181012

---
+++ ljftest_cifar10_SE-ResNet-20/train_net.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+CAFFE_HOME=../..
+
+SOLVER=./solver.prototxt
+#SNAPSHOT=./models/gnet_yolo_region_darknet_v3_multifixed_iter_2000.solverstate
+#WEIGHTS=./yolo.caffemodel
+#SNAPSHOT=./models/gnet_yolo_region_darknet_v3_pretrain_rectify_iter_120000.solverstate
+$CAFFE_HOME/build/tools/caffe train \
+    --solver=$SOLVER --weights=$WEIGHTS --gpu=0 \
+    2>&1 | tee train_SE-ResNet-20.log #--weights=$WEIGHTS #--gpu=0,1
+
